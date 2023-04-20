920fdd96584a56eed4bb3a3ddb861788278665d3
Ssaanndd <sandrineb.dev@gmail.com>
2023-04-20 11:41:25 +0200

    affichage date heure

---
+++ affichagedateheure.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Hello world ! </h1>
+        <p>Quand tu perds la mémoire ou te réveille d'un long sommeil après le bisou du prince charmant, tout ce que que tu veux savoir c'est : aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+    </body>
+</html>
